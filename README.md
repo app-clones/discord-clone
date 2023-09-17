04fbaacc2e01954dcb0c2d7f0c86a3cc47a8bce1
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-09-17 09:49:42 -0700

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Discord Clone
+
+Discord clone made using Next.js 13, Tailwind CSS, Socket.io, Prisma, and MySQL
+
+# Demo
+
+Soon
+
+# Credit
+
+[This](https://www.youtube.com/watch?v=ZbX4Ok9YX94)
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <p className="text-3xl font-bold text-indigo-500">Hello World</p>
+            <Button className="bg-indigo-500">Click me</Button>
+        </div>
+    );
+}
